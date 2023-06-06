f6d5c3699b2379ae3279da768fcbbab383c3ca11
RossiJr <josefernando53@hotmail.com>
2023-06-06 10:29:58 -0300

    Força bruta funcional

--- app/src/força_bruta.py
+++ app/src/força_bruta.py
@@ -1,39 +1,6 @@
 import math
 import itertools
 
-if __name__ == "__main__":
-    stores = []
-    stores_obj = []
-    truck_products = []
-
-    # Open and read file
-    with open("lojas.txt", "r") as file:
-        for line in file:
-            stores.append(line.replace("\n", ""))
-
-    # Create a list of stores with their attributes
-    for store in stores:
-        store_aux = store.split(" ")
-        stores_obj.append(
-            {
-                "number": store_aux[0],
-                "x": int(store_aux[1]),
-                "y": int(store_aux[2]),
-                "destination_stores": store_aux[3:] if len(store_aux) > 2 else [],
-            }
-        )
-
-    origin_store = stores_obj[0]
-    route = [].append(
-        stores_obj.filter(lambda store: store.get("destination_stores") > 0)[0]
-    )
-    print(route)
-for i in range(1, len(stores_obj)):
-    for j in range(1, len(stores_obj)):
-        if i != j:
-            pass
-
-    # Calculate the distance between two points
 
 
 def calcular_distancia(x1, y1, x2, y2):
@@ -47,68 +14,46 @@ def calcular_gasto_combustivel(distancia, num_produtos):
     return distancia / rendimento
 
 
-def encontrar_proxima_loja(loja_atual, lojas, rota):
-    distancia_minima = math.inf
-    proxima_loja = None
-
-    # Iterar sobre todas as lojas para calcular a rota do caminhão
-    for loja in lojas:
-        # Verificar se a loja atual não está presente na rota percorrida
-        if loja not in rota:
-            # Verificar se a distância calculada é menor que a distância mínima encontrada até agora
-            distancia = calcular_distancia(
-                loja_atual["x"], loja_atual["y"], loja["x"], loja["y"]
-            )
-            if distancia < distancia_minima:
-                distancia_minima = distancia
-                proxima_loja = loja
-
-    return proxima_loja, distancia_minima
-
-
 def calcular_rota_caminhao(lojas, carga_caminhao):
     rota = []
-    distancia_total = 0
+    gastoTotal = 0
     carga = 0
+    destinos = []
 
     loja_atual = lojas[0]
+    for dest in loja_atual['destination_stores']:
+        destinos.append(dest)
     rota.append(loja_atual)
 
-    while True:
-        proxima_loja, distancia = encontrar_proxima_loja(loja_atual, lojas, rota)
+    for loja in lojas[1:]:
+        distancia = calcular_distancia(loja_atual['x'], loja_atual['y'], loja['x'], loja['y'])
+        gasto_combustivel = calcular_gasto_combustivel(distancia, carga)
 
-        if proxima_loja is None:
-            # Todas as lojas foram visitadas, retornar ao ponto de partida
-            # Adiciona a distância de retorno ao ponto de partida à distância total percorrida
-            distancia_total += calcular_distancia(
-                loja_atual["x"], loja_atual["y"], lojas[0]["x"], lojas[0]["y"]
-            )
-            # Adiciona a loja inicial à rota
-            rota.append(lojas[0])
-            break
-
-        distancia_total += distancia
-        carga += len(loja_atual["destination_stores"]) # Atualiza a carga do caminhão com o número de destinos da loja atual
+        print(destinos)
+        if loja['number'] in destinos:
+            # Remover loja da lista de destinos
+            destinos.remove(loja['number'])
+            carga -= 1
+        for dest in loja['destination_stores']:
+            destinos.append(dest)
+        gastoTotal += gasto_combustivel
+        carga += len(loja["destination_stores"]) # Atualiza a carga do caminhão com o número de destinos da loja atual
 
         # A carga do caminhão excedeu a capacidade, retornar ao ponto de partida
         if carga >= carga_caminhao:
-            distancia_total += calcular_distancia(
-                loja_atual["x"], loja_atual["y"], lojas[0]["x"], lojas[0]["y"]
-            )
-            rota.append(lojas[0])
-            carga = 0
+            print('carga excedeu')
+        
+        # Adiciona a próxima loja à rota
+        rota.append(loja)
+        # Atualiza a loja atual para a próxima loja
+        loja_atual = loja
 
-    # Adiciona a próxima loja à rota
-    rota.append(proxima_loja)
-    # Atualiza a loja atual para a próxima loja
-    loja_atual = proxima_loja
-
-    return rota, distancia_total
+    return rota, gastoTotal, carga
 
 
 # Ler informações das lojas do arquivo
 lojas = []
-with open("lojas.txt", "r") as file:
+with open("D:\\PAA\\trab2\\caxeiro2\\caxeiro-viajante-paa\\app\\src\\lojas.txt", "r") as file:
     for line in file:
         store_info = line.split()
         loja = {
@@ -131,18 +76,16 @@ melhor_gasto_combustivel = math.inf
 
 # Calcular rota e gasto de combustível para cada permutação
 for permutacao in permutacoes:
-    destinos_permutados = [lojas[0]] + list(permutacao) + [lojas[0]]
-    rota, distancia_total = calcular_rota_caminhao(destinos_permutados, carga_caminhao)
-    gasto_combustivel = calcular_gasto_combustivel(distancia_total, carga_caminhao)
+    destinos_permutados = [lojas[0]] + list(permutacao)
+    rota, gasto_total, carga = calcular_rota_caminhao(destinos_permutados, carga_caminhao)
 
-    if gasto_combustivel < melhor_gasto_combustivel:
+    if carga == 0 and gasto_total < melhor_gasto_combustivel:
         melhor_rota = rota
-        melhor_gasto_combustivel = gasto_combustivel
-
+        melhor_gasto_combustivel = gasto_total
+    print(f"{permutacao}  -  {carga}")
 # Imprimir resultados
 print("Rota do caminhão:")
 for loja in melhor_rota:
-    print(loja["number"], loja["x"], loja["y"])
+    print(loja["number"], loja["x"], loja["y"], carga)
 
-print("Distância total percorrida:", distancia_total)
-print("Melhor gasto de combustível:", melhor_gasto_combustivel)
+print("Melhor gasto de combustível:", gasto_total)
